8305422f209e2156d3f41efe444435359a6eb570
Zahin52 <zahinuddin5@gmail.com>
2021-10-30 23:39:04 +0600

    booking cancle added

--- index.js
+++ index.js
@@ -100,6 +100,12 @@ async function run() {
          const result = await destinoCollection.deleteOne(query)
          res.json(result)
       })
+      app.delete('/bookings/:id', async (req, res) => {
+         const id = req.params.id
+         const query = { _id: ObjectId(id) }
+         const result = await destinoBookingCollection.deleteOne(query)
+         res.json(result)
+      })
    } finally {
       // await client.close();
    }
